2898a17e372bf62b53854d55594201cde63bac93
Yaotecatl Martinez <72234697+YaotecatlMtz@users.noreply.github.com>
2023-10-09 21:39:23 -0600

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-function cleanStr(str){
-    str = str.split('/')[1];
-    return str = str.split('.')[0];;
-}
-
-let cadena = 'src/espinosaurio.jpg';
-cadena = cleanStr(cadena);
-console.log(cadena);
